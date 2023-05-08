44c39a7dec1dbad4abf3b627734bf5d249fddae5
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2023-05-08 20:26:48 +0900

    add column

--- init.sql
+++ init.sql
@@ -4,6 +4,7 @@ CREATE TABLE `user` (
   `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `email` varchar(300) COLLATE utf8mb4_bin NOT NULL,
+  `authority` varchar(300) COLLATE utf8mb4_bin NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_flag` tinyint(3) unsigned NOT NULL,
