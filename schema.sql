4c447612d4443c849955390bba797c4b8329f82c
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2023-05-10 20:53:41 +0900

    delete table

--- schema.sql
+++ schema.sql
@@ -2,19 +2,9 @@ USE test_db;
 
 CREATE TABLE `user` (
   `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `school_id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `email` varchar(300) COLLATE utf8mb4_bin NOT NULL,
   `authority` varchar(300) COLLATE utf8mb4_bin NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_flag` tinyint(3) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `school` (
-  `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `deleted_flag` tinyint(3) unsigned NOT NULL,
