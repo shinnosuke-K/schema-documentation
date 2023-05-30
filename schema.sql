bc7fbffbbd0b0de925a6e4d0c6226047b931539e
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2023-05-30 12:49:46 +0900

    change name

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 USE test_db;
 
-CREATE TABLE `user` (
+CREATE TABLE `student` (
   `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
   `school_id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
