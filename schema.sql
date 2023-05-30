ce22ca2141df0275a09f344e069aaf81e26916a9
shinnosuke-K <shinnosuke.kishida@so-tech.co.jp>
2023-05-30 11:54:46 +0900

    add test table

--- schema.sql
+++ schema.sql
@@ -41,3 +41,14 @@ CREATE TABLE `curriculum` (
   `deleted_flag` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `regular_test` (
+  `id` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_flag` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
